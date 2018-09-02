27e7dc18903b86d6473b147bb539a81408fe3f89
JanhonChen <lantize@gmail.com>
2018-09-02 19:15:05 +0800

    Revert "9/2 埋入log偵錯檔,使用Firebase-Crashlytics ." 更新計時器功能.

--- app/src/main/java/com/example/janhon/bonusrunningbeforeyou/Running/RunningDataActivity.java
+++ app/src/main/java/com/example/janhon/bonusrunningbeforeyou/Running/RunningDataActivity.java
@@ -3,48 +3,50 @@ package com.example.janhon.bonusrunningbeforeyou.Running;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
+import com.crashlytics.android.Crashlytics;
 import com.example.janhon.bonusrunningbeforeyou.R;
+
+import io.fabric.sdk.android.Fabric;
 
 // CP102 陳建宏
 public class RunningDataActivity extends Activity {
-    TextView timerTextView;
-    long startTime ;
+    Long mRecordTime ;
 
+    public void enableDebugMode() {
+        // [START crash_enable_debug_mode]
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(new Crashlytics())
+                .debuggable(true)  // Enables Crashlytics debugger
+                .build();
+        Fabric.with(fabric);
+        // [END crash_enable_debug_mode]
+    }
 
-    //runs without a timer by reposting this handler at the end of the runnable
-    Handler timerHandler = new Handler();  //將訊息與執行緒合併
-    Runnable timerRunnable = new Runnable() {  //新執行緒
-        //抓時間的類別.TimeStamp
-
-        @Override
-        public void run() {   //內部匿名類別
-            long millis = System.currentTimeMillis() - startTime;
-            int seconds = (int) (millis / 1000);
-            int minutes = seconds / 60;
-            seconds = seconds % 60;
-            int hours = minutes /60;
-            minutes = minutes % 60 ;
-            timerTextView.setText(String.format("%d:%02d:%02d",hours, minutes, seconds));
-            timerHandler.postDelayed(this, 500);
-        }
-    };
-
-
+    public void enableAtRuntime() {
+        // [START crash_enable_at_runtime]
+        Fabric.with(this, new Crashlytics());
+        // [END crash_enable_at_runtime]
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running_data);
-
-        startTime = System.currentTimeMillis();   //設定起始時間為0
-        timerHandler.postDelayed(timerRunnable, 0);
-        timerTextView = findViewById(R.id.text);
+        Chronometer text_timer = findViewById(R.id.text_timer);
+        text_timer.setBase(SystemClock.elapsedRealtime());
+        text_timer.start();
+        enableDebugMode();
+        enableAtRuntime();
+        Crashlytics.log(Log.DEBUG, "tag", "message");
         Button button = findViewById(R.id.btPause);
-        Button button1 = findViewById(R.id.btPlay);
+        Button button1 =  findViewById(R.id.btPlay);
         button1.setVisibility(button1.GONE);
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -55,25 +57,30 @@ public class RunningDataActivity extends Activity {
         });
     }
 
+
+
+
     public void onPauseClick(View view) {
-        super.onPause();
+        Chronometer text_timer = findViewById(R.id.text_timer);
+        text_timer.stop();
+        mRecordTime = SystemClock.elapsedRealtime();
         Button button = findViewById(R.id.btPause);  //將暫停鈕移除
         button.setVisibility(View.GONE);
-        timerHandler.removeCallbacks(timerRunnable);  //移除執行緒
         Button button1 = findViewById(R.id.btPlay);  //將繼續鈕置入
         button1.setVisibility(View.VISIBLE);
-
-
     }
 
     public void onPlayClick(View view) {
-
-        timerHandler.postDelayed(timerRunnable, 0);
-        timerTextView = findViewById(R.id.text);
+        Chronometer text_timer = findViewById(R.id.text_timer);
+        if (mRecordTime != 0) {
+            text_timer.setBase(text_timer.getBase() + (SystemClock.elapsedRealtime() - mRecordTime));
+        } else {
+            text_timer.setBase(SystemClock.elapsedRealtime());
+        }
+        text_timer.start();
         Button button1 = findViewById(R.id.btPlay);
         button1.setVisibility(View.GONE);
-        Button button = findViewById(R.id.btPause);
+        Button button =  findViewById(R.id.btPause);
         button.setVisibility(View.VISIBLE);
     }
-
 }
